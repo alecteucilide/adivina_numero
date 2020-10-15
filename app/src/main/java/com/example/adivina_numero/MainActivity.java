34719568c0691a79abf7884efa98794fd34b3cf4
alecteucilide <alecadrianteucilide@gmail.com>
2020-10-15 19:00:59 +0200

    App with ranking text view done

--- app/src/main/java/com/example/adivina_numero/MainActivity.java
+++ app/src/main/java/com/example/adivina_numero/MainActivity.java
@@ -11,10 +11,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String name = new String();
+    int numAttempts = 0;
+    int secretNum = 0;
+    boolean playing = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonNumber = findViewById(R.id.buttonNumber);
+        EditText enteredNumber = findViewById(R.id.textInPutNumber);
 
         final Button buttonName = findViewById(R.id.buttonName);
         buttonName.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +29,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText enteredName = findViewById(R.id.textInPutName);
                 name = enteredName.getText().toString();
-                Toast.makeText(MainActivity.this, "Name registered.", Toast.LENGTH_SHORT).show();
+                if(name.equals("") || name == null){
+                    Toast.makeText(MainActivity.this, "Invalid name.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Name registered.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -30,12 +41,32 @@ public class MainActivity extends AppCompatActivity {
         buttonRanking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!(name == null) && !name.equals("")){
-                    //Toast.makeText(MainActivity.this, "Your name is "+name, Toast.LENGTH_SHORT).show();
-                    callRanking();
+                if(numAttempts > 0){
+                    if(playing==false){
+                        if(!(name == null) && !name.equals("")){
+                            //Toast.makeText(MainActivity.this, "Your name is "+name, Toast.LENGTH_SHORT).show();
+                            callRanking();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Name not valid."+name, Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "End current game before accessing ranking."+name, Toast.LENGTH_SHORT).show();
+                    }
                 }else{
-                    Toast.makeText(MainActivity.this, "Name not valid."+name, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You have to play at least once to acces ranking."+name, Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        final Button buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Game started!", Toast.LENGTH_SHORT).show();
+                numAttempts=0;
+                name="";
+                secretNum = (int)(Math.random()*1)+1;
+                play(buttonNumber, enteredNumber, secretNum);
             }
         });
     }
@@ -43,6 +74,35 @@ public class MainActivity extends AppCompatActivity {
     public void callRanking(){
         Intent intent = new Intent(this, MainActivity_Ranking.class);
         intent.putExtra("name", name);
+        intent.putExtra("numAttempts", numAttempts);
         startActivity(intent);
+    }
+
+    public void play(Button buttonNumber, EditText enteredNumber, int secretNumber){
+        buttonNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playing = true;
+                EditText enteredNum = (EditText) findViewById (R.id.textInPutNumber);
+                String enteredNumString = enteredNum.getText().toString();
+
+                try{
+                    int enteredNumInt = Integer.parseInt(enteredNumString);
+                    numAttempts++;
+                    if(enteredNumInt<secretNum){
+                        Toast.makeText(MainActivity.this, "The sercret number is bigger.", Toast.LENGTH_SHORT).show();
+                    }else if(enteredNumInt>secretNum) {
+                        Toast.makeText(MainActivity.this, "The secret number is smaller.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, "You have found the secret number in "+numAttempts+" attempts.", Toast.LENGTH_SHORT).show();
+                        playing = false;
+                        return;
+                    }
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Invalid entry.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/adivina_numero/MainActivity_Ranking.java
+++ app/src/main/java/com/example/adivina_numero/MainActivity_Ranking.java
@@ -12,14 +12,17 @@ import java.util.ArrayList;
 
 public class MainActivity_Ranking extends AppCompatActivity {
     static ArrayList<String> arrayNames = new ArrayList<String>();
-    String name = new String();
+    static ArrayList<Integer> arrayNumAttempts = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__ranking);
         TextView tvRanking = findViewById(R.id.tvRanking);
         arrayNames.add(getIntent().getStringExtra("name"));
+        arrayNumAttempts.add(getIntent().getIntExtra("numAttempts", 0));
 
+        orderArrays(arrayNames, arrayNumAttempts);
         printRanking(tvRanking);
 
 
@@ -39,10 +42,25 @@ public class MainActivity_Ranking extends AppCompatActivity {
     }
 
     public void printRanking(TextView tvRanking){
-        String names = new String();
-        for (String n : arrayNames){
-            names = names+"\n"+n;
+        String scores = new String();
+        for (int i = 0; i<arrayNames.size();i++){
+            scores = scores+"\n"+arrayNames.get(i)+"          "+arrayNumAttempts.get(i);
         }
-        tvRanking.setText(names);
+        tvRanking.setText(scores);
+    }
+
+    public void orderArrays(ArrayList<String> arrayNames, ArrayList<Integer> arrayNumAttempts){
+        for(int i = arrayNumAttempts.size()-1; i>=1; i--){
+            for(int j = 0; j < i; j++){
+                if(arrayNumAttempts.get(j)>arrayNumAttempts.get(i)){
+                    int numChange = arrayNumAttempts.get(j);
+                    arrayNumAttempts.set(j, arrayNumAttempts.get(i));
+                    arrayNumAttempts.set(i, numChange);
+                    String nameChange = arrayNames.get(j);
+                    arrayNames.set(j, arrayNames.get(i));
+                    arrayNames.set(i, nameChange);
+                }
+            }
+        }
     }
 }
