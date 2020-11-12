83ee5f3e704f4dbe654365181983ef1d4a13acbd
alecteucilide <alecadrianteucilide@gmail.com>
2020-11-12 19:19:55 +0100

    functions for persistence ready

--- app/src/main/java/com/example/adivina_numero/MainActivity_Ranking.java
+++ app/src/main/java/com/example/adivina_numero/MainActivity_Ranking.java
@@ -1,9 +1,7 @@
 package com.example.adivina_numero;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +9,21 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class MainActivity_Ranking extends AppCompatActivity {
     static ArrayList<Match> arrayMatch = new ArrayList<Match>();
@@ -47,6 +57,84 @@ public class MainActivity_Ranking extends AppCompatActivity {
         });
 
     }
+
+    public void createFile(){
+        File path = getFilesDir();
+        File file = new File(path.getPath()+"/matchs.xml");
+    }
+
+    public void readMatchs() {
+        File path = getFilesDir();
+        File file = new File(path.getPath()+"/matchs.xml");
+        arrayMatch.clear();
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("match");
+
+            for(int index = 0; index < nList.getLength(); index++) {
+                Node nNode = nList.item(index);
+                String name;
+                int score;
+                int time;
+                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    name = eElement.getElementsByTagName("name").item(0).getTextContent();
+                    score = Integer.parseInt(eElement.getElementsByTagName("score").item(0).getTextContent());
+                    time = Integer.parseInt(eElement.getElementsByTagName("time").item(0).getTextContent());
+                    Match m = new Match(name, score, time);
+                    arrayMatch.add(m);
+                }
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveMatchs(ArrayList<Match> arrayMatch){
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            // definimos el elemento raíz del documento
+            Element eRaiz = doc.createElement("matchs");
+            doc.appendChild(eRaiz);
+
+            for(Match m: arrayMatch){
+                Element eMatch = doc.createElement("match");
+                eRaiz.appendChild(eMatch);
+
+                Element eName = doc.createElement("name");
+                eName.appendChild(doc.createTextNode(m.getName()));
+                eMatch.appendChild(eName);
+
+                Element eScore = doc.createElement("score");
+                eScore.appendChild(doc.createTextNode(String.valueOf(m.getScore())));
+                eMatch.appendChild(eScore);
+
+                Element eTime = doc.createElement("time");
+                eTime.appendChild(doc.createTextNode(String.valueOf(m.getTime())));
+                eMatch.appendChild(eTime
+                );
+            }
+
+            // clases necesarias finalizar la creación del archivo XML
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("matchs.xml"));
+
+            transformer.transform(source, result);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void addRowToTable(TableLayout tlRanking, String name, Integer score, Integer time){
         TableRow tr = new TableRow(this);
