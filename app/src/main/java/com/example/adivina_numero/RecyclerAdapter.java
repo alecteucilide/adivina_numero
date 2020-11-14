b0713b746de749ae090aebf8c1c9b7d33534a962
alecteucilide <alecadrianteucilide@gmail.com>
2020-11-14 13:54:41 +0100

    app finished, add comments and change default secret number

--- app/src/main/java/com/example/adivina_numero/RecyclerAdapter.java
+++ app/src/main/java/com/example/adivina_numero/RecyclerAdapter.java
@@ -1,13 +1,17 @@
 package com.example.adivina_numero;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
@@ -31,7 +35,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         holder.tvName.setText(arrayMatch.get(position).getName());
         holder.tvScore.setText(String.valueOf(arrayMatch.get(position).getScore()));
         holder.tvTime.setText(String.valueOf(arrayMatch.get(position).getTime()));
-        holder.tvPhotoPath.setText(arrayMatch.get(position).getPhotoPath());
+        holder.ivPhoto.setImageBitmap(getPhotoBitmap(position));
     }
 
     @Override
@@ -40,13 +44,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
-        TextView tvName, tvScore, tvTime, tvPhotoPath;
+        TextView tvName, tvScore, tvTime;
+        ImageView ivPhoto;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvName = itemView.findViewById(R.id.tvName);
             tvScore = itemView.findViewById(R.id.tvScore);
             tvTime = itemView.findViewById(R.id.tvTime);
-            tvPhotoPath = itemView.findViewById(R.id.tvPhotoPath);
+            ivPhoto = itemView.findViewById(R.id.ivPhoto);
         }
+    }
+
+    public Bitmap getPhotoBitmap(int matchIndex){
+        File fPhotoPath = new File(arrayMatch.get(matchIndex).getPhotoPath());
+        Bitmap photoBitmap = BitmapFactory.decodeFile(fPhotoPath.getAbsolutePath());
+        return photoBitmap;
     }
 }
